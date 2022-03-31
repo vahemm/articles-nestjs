94fd88320f43e7c5586497ac61dde70077844ce6
Vahe Manukyan <vahemm@outlook.com>
2022-03-31 12:30:49 +0400

    Add usersModule and authenticationModule

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import PostsService from './posts.service';
 import { CreatePostDto } from './dto/createPost.dto';
 import { UpdatePostDto } from './dto/updatePost.dto';
+import JwtAuthenticationGuard from 'src/authentication/jwt-authentication.guard';
 
 @Controller('posts')
 export default class PostsController {
@@ -20,13 +21,14 @@ export default class PostsController {
     }
 
     @Post()
+    @UseGuards(JwtAuthenticationGuard)
     async createPost(@Body() post: CreatePostDto) {
         return this.postsService.createPost(post);
     }
 
     @Put(':id')
     async replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
-        return this.postsService.replacePost(Number(id), post);
+        return this.postsService.updatePost(Number(id), post);
     }
 
     @Delete(':id')
