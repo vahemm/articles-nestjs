b135d086dbdffb430ea53f43147e57cd2d0df4ba
Vahe Manukyan <vahemm@outlook.com>
2022-04-01 16:10:18 +0400

    Add error handling and data validation

--- src/authentication/dto/register.dto.ts
+++ src/authentication/dto/register.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsString, IsNotEmpty, MinLength } from 'class-validator';
+ 
 export class RegisterDto {
-    email: string;
-    name: string;
-    password: string;
-  }
-   
+  @IsEmail()
+  email: string;
+ 
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+ 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(7)
+  password: string;
+}
+ 
+export default RegisterDto;
